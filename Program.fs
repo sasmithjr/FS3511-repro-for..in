35b6d9866abf07edb3cf3b86f647a8cbac8eba31
Stephen Smith <sasmithjr@gmail.com>
2022-08-09 17:41:09 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿let values = [1;2;3]
+
+let a =
+    task {
+        for value in values do
+            printfn $"{value}"
+    }
+
+let v =
+    task {
+        for value in [1;2;3] do
+            printfn $"{value}"
+    }
+
+printfn "Hello from F#"
