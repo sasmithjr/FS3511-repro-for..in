61a0084c3f817ee928f6038b4259ab00c7348833
Stephen Smith <sasmithjr@gmail.com>
2022-09-14 08:26:02 -0400

    Adds comment

--- Program.fs
+++ Program.fs
@@ -2,8 +2,9 @@
     let errorFn () =
         task {
             // let values = [1;2;3;4]
-            // for (value) in values do
+            // for (value) in values do // no warning
             //     printfn $"{value}"
+            
             let values = [|1,'a'; 2,'b'; 3,'c'; 4,'d'|]
             for (value, letter) in values do // warning here
                 printfn $"{value}:{letter}"
