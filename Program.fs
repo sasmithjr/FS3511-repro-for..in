0bdbfc14ca2758b9ee42f900967b3f6061392f14
Stephen Smith <sasmithjr@gmail.com>
2022-09-14 08:22:50 -0400

    Adds tuples to for loop

--- Program.fs
+++ Program.fs
@@ -1,15 +1,12 @@
-﻿let values = [1;2;3]
-
-let a =
-    task {
-        for value in values do
-            printfn $"{value}"
-    }
-
-let v =
-    task {
-        for value in [1;2;3] do
-            printfn $"{value}"
-    }
-
+﻿module NotTopLevel =
+    let errorFn () =
+        task {
+            // let values = [1;2;3;4]
+            // for (value) in values do
+            //     printfn $"{value}"
+            let values = [1,'a'; 2,'b'; 3,'c'; 4,'d']
+            for (value, letter) in values do
+                printfn $"{value}:{letter}"
+        }
+    
 printfn "Hello from F#"
