cafa3e644cdd97e26a10bc55215eea31b1d7dea5
Stephen Smith <sasmithjr@gmail.com>
2022-09-14 08:25:13 -0400

    Adds check for not matching on tuple

--- Program.fs
+++ Program.fs
@@ -4,9 +4,11 @@
             // let values = [1;2;3;4]
             // for (value) in values do
             //     printfn $"{value}"
-            let values = [1,'a'; 2,'b'; 3,'c'; 4,'d']
-            for (value, letter) in values do
+            let values = [|1,'a'; 2,'b'; 3,'c'; 4,'d'|]
+            for (value, letter) in values do // warning here
                 printfn $"{value}:{letter}"
+            // for pair in values do // Also provides a warning
+            //     printfn $"{fst pair}:{snd pair}"
         }
     
 printfn "Hello from F#"
